58ca1e2a13b8561c5d5b4b3d5fd17dab76016e2f
Eren <erenergen96@gmail.com>
2023-05-20 19:29:28 +0300

    hehe

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import streamlit as st
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
 from stack_predict_page import show_stack_predict_page
 from cyber_predict_page import show_cyber_predict_page
-import urllib3.contrib.pyopenssl
+import streamlit as st
 
 
-urllib3.contrib.pyopenssl.inject_into_urllib3()
 
 #show_cyber_predict_page()
 
